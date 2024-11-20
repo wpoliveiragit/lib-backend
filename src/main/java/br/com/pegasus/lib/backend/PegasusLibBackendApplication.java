7f0496a74115a609554d95c0fa5d5fbb5473a3ce
wpoliveiracel <wp.oliveira.cel@gmail.com>
2024-11-20 06:55:33 -0300

    add template projeto

---
+++ src/main/java/br/com/pegasus/lib/backend/PegasusLibBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.pegasus.lib.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PegasusLibBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PegasusLibBackendApplication.class, args);
+	}
+
+}
